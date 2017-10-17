a88d19b69ad3fcf0077a836777c7ef424465f294
Tim Matthews <tim@3sidedcube.com>
2017-10-17 09:07:33 +0100

    Fixed to work with RN 0.47 on Android

--- android/src/main/java/io/card/react/ReactCardIOPackage.java
+++ android/src/main/java/io/card/react/ReactCardIOPackage.java
@@ -20,7 +20,6 @@ public class ReactCardIOPackage implements ReactPackage {
         );
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
